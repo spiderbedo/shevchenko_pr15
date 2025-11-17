1e37e021a51067bb50cb6b16f2b1914599fccee8
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-11-18 00:36:35 +0700

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,1 +1,13 @@
+def mod_number(a, b):
+    if b == 0:
+        raise ValueError("Нельзя делить на ноль")
+    if a >= 0:
+        if a < abs(b):
+            return a
+        return mod_number(a - abs(b), abs(b))
+    else:
+        return mod_number(a + abs(b), abs(b))
 
+
+if __name__ == "__main__":
+    print(mod_number(int(input()), int(input())))
