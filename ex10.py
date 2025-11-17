85fefd369ca428d8a2f9dc2a8f86df6f4401d1a0
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-11-18 01:34:27 +0700

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -1,1 +1,11 @@
+def maxlist(a) -> int:
+    if len(a) == 1:
+        return a[0]
+    if a[0] > maxlist(a[1:]):
+        return a[0]
+    else:
+        maxlist(a[1:])
 
+
+if __name__ == "__main__":
+    print(maxlist([8377, 7, 245, 9999999, 202022, 936253, 54539287, 0, 11]))
