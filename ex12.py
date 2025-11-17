c98c2ce9ec79666e3bb1bc99b28f8566465141fd
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-11-18 01:44:07 +0700

    Update ex12.py

--- ex12.py
+++ ex12.py
@@ -1,1 +1,11 @@
+def search(a, x) -> int:
+    if not a:
+        return 0
+    if a[0] == x:
+        return 1
+    return search(a[1:], x)
 
+
+if __name__ == "__main__":
+    print(search([8377, 7, 245, 9999999, 202022, 936253, 54539287, 0, 11], 245))
+    print(search([8377, 7, 245, 9999999, 202022, 936253, 54539287, 0, 11], -117722883399))
