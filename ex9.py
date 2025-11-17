63ab3c9c385b4a8cc8422e592c568fe407f3e2d2
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-11-18 01:20:42 +0700

    Update ex9.py

--- ex9.py
+++ ex9.py
@@ -1,1 +1,8 @@
+def combin(n, k) -> int:
+    if n == k or k == 0:
+        return 1
+    return combin(n - 1, k - 1) + combin(n - 1, k)
 
+
+if __name__ == "__main__":
+    print(nod(int(input()), int(input())))
