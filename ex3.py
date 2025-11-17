78d21b7d6c8b920743fedeb949acc1c4799577be
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-11-17 20:13:45 +0700

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,1 +1,8 @@
+def progress(a1, r, n):
+    if n == 1:
+        return a1
+    return progress(a1, r, n - 1) + r
 
+
+if __name__ == "__main__":
+    print(progress(int(input()), int(input()), int(input())))
