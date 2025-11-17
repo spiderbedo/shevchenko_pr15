2e9ad489b86c725a5e556efe9d2e77af02965f23
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-11-18 00:46:15 +0700

    Update ex6.py

--- ex6.py
+++ ex6.py
@@ -1,1 +1,10 @@
+def degree5(n) -> int:
+    if n == 5:
+        return 1
+    elif n % 5 != 0 and n % 10 != 0:
+        return -1
+    return degree5(n // 5) + 1
 
+
+if __name__ == "__main__":
+    print(degree5(int(input())))
