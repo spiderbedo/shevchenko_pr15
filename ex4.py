9b90e3c27ed1ed8936c72cb8c406df4cbf986fdd
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-11-18 00:24:06 +0700

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -1,1 +1,8 @@
+def sum_progress(a1, r, n):
+    if n == 1:
+        return a1
+    return sum_progress(a1, r, n - 1) + (a1 + (n-1) * r)
 
+
+if __name__ == "__main__":
+    print(sum_progress(int(input()), int(input()), int(input())))
