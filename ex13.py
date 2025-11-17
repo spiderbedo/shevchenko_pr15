8a0123beae6f298b12ad9523369675042f0ce37c
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-11-18 01:54:03 +0700

    Update ex13.py

--- ex13.py
+++ ex13.py
@@ -1,1 +1,11 @@
+def odd_list(a, n) -> list:
+    if n == 0:
+        return []
+    lol = odd_list(a, n-1)
+    if a[n-1] % 2 == 0:
+        lol.append(a[n-1])
+    return lol
 
+
+if __name__ == "__main__":
+    print(odd_list([8377, 7, 245, 9999999, 202022, 936253, 54539287, 11], 8))
