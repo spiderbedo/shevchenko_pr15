c4dc1331e20b9845fb365ab2d772b2a7aa2b5c76
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-11-18 01:38:25 +0700

    Add function to find index of maximum in list

--- ex11.py
+++ ex11.py
@@ -1,1 +1,10 @@
+def ind_maxlist(a) -> int:
+    if len(a) == 1:
+        return 0
+    idx_tail = ind_maxlist(a[1:])
+    idx_tail += 1
+    return 0 if a[0] >= a[idx_tail] else idx_tail
 
+
+if __name__ == "__main__":
+    print(ind_maxlist([8377, 7, 245, 9999999, 202022, 936253, 54539287, 0, 11]))
