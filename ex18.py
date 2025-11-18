1ab5f1b5de24ef5ca23650e57208d5b2da970fdb
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-11-18 09:26:29 +0700

    Update ex18.py

--- ex18.py
+++ ex18.py
@@ -1,1 +1,11 @@
+def simmetr(s, i, j) -> None:
+    if i >= j:
+        return True
+    if s[i] != s[j]:
+        return False
+    return simmetr(s, i + 1, j - 1)
 
+
+if __name__ == "__main__":
+    st = input()
+    print(simmetr(st, 0, len(st)-1))
