df6584dc556d3190c601f7d67939f6bcd954ebf2
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-11-18 09:18:00 +0700

    Update ex17.py

--- ex17.py
+++ ex17.py
@@ -1,1 +1,18 @@
+import math
 
+def is_prime_rec(x, d):
+    if d > math.sqrt(x):
+        return 1
+    if x % d == 0:
+        return 0
+    return is_prime_rec(x, d + 1)
+
+
+def function1(x):
+    if x < 2:
+        return 0
+    return is_prime_rec(x, 2)
+
+
+if __name__ == "__main__":
+    print(function1(int(input())))
