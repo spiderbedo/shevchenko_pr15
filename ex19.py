f78c6f36d13989e016a7d34e821c3bdd618303f0
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-11-18 09:34:09 +0700

    Update ex19.py

--- ex19.py
+++ ex19.py
@@ -1,1 +1,10 @@
+def count(a, b) -> int:
+    if a == 0 or b == 0:
+        return 0
+    if a < b:
+        a, b = b, a
+    return a // b + count(b, a % b)
 
+
+if __name__ == "__main__":
+    print(count(int(input()), int(input())))
