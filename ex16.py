c33f59c72bc5018aebe9e8c6b90e2a4fdda10750
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-11-18 09:07:46 +0700

    Update ex16.py

--- ex16.py
+++ ex16.py
@@ -1,1 +1,9 @@
+def ten_to_n(x, n) -> str:
+    digits = "0123456789ABCDEF"
+    if x == 0:
+        return ""
+    return ten_to_n(x // n, n) + digits[x % n]
 
+
+if __name__ == "__main__":
+    print(ten_to_n(int(input()), int(input())))
