a7c7f5bd19b084974a1bf3b98cc06b3c45d7bfd7
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-11-18 09:19:19 +0700

    Rename recursive function to is_prime

--- ex17.py
+++ ex17.py
@@ -1,17 +1,17 @@
 import math
 
-def is_prime_rec(x, d) -> int:
+def is_prime(x, d) -> int:
     if d > math.sqrt(x):
         return 1
     if x % d == 0:
         return 0
-    return is_prime_rec(x, d + 1)
+    return is_prime(x, d + 1)
 
 
 def function1(x) ->int:
     if x < 2:
         return 0
-    return is_prime_rec(x, 2)
+    return is_prime(x, 2)
 
 
 if __name__ == "__main__":
